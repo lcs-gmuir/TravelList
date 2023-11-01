ff31c1df7984ea6e6fb37e2c32398ff31734b780
Griffin Muir <gmuir@lcs.on.ca>
2023-11-01 19:43:44 -0400

    made some folders to organize my code, deleted content view and made a structure to contain the list item

---
+++ TravelList/views/listItem .swift
@@ -0,0 +1,18 @@
+//
+//  listItem .swift
+//  TravelList
+//
+//  Created by Griffin Muir on 2023-11-01.
+//
+
+import SwiftUI
+
+struct listItem_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listItem_()
+}
---
+++ TravelList/views/listView.swift
@@ -0,0 +1,18 @@
+//
+//  listView.swift
+//  TravelList
+//
+//  Created by Griffin Muir on 2023-11-01.
+//
+
+import SwiftUI
+
+struct listView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listView()
+}
