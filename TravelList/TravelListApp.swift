4ab8c19bd573f70fdd79d04819efe25906220ab8
Griffin Muir <gmuir@lcs.on.ca>
2023-11-01 12:20:25 -0400

    Initial Commit

---
+++ TravelList/TravelListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelListApp.swift
+//  TravelList
+//
+//  Created by Griffin Muir on 2023-11-01.
+//
+
+import SwiftUI
+
+@main
+struct TravelListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
